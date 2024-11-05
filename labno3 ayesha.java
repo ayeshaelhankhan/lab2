566fc0970a3a3f62caabf57068c37e78112d55a6
AyeshaElhan <ayeshakhanelhan@gmail.com>
2024-11-05 09:29:53 +0500

    lab2 first commit

---
+++ labno3 ayesha.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Account{
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    //constructor
+
+    public Account(String accountNumber,String accountholderName,double balance ) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+
+    public String getAccountNumber() {
+        return accountNumber;
+
+    }
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+    public double getbalance() {
+        return balance;
+
+    }
+
+}
+public class account {
+    
+}
